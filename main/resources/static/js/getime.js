f8b8150afd69556d2adcf158e13c3a70ae485104
leeikjeun <dldlrwjs12@gmail.com>
2017-06-16 12:57:09 +0900

    easy

---
+++ main/resources/static/js/getime.js
@@ -0,0 +1,28 @@
+/**
+ * Created by adaeng on 2017. 6. 16..
+ */
+
+function getTimeStamp(time) {
+    var d = new Date(time);
+    var s =
+        leadingZeros(d.getFullYear(), 4) + '-' +
+        leadingZeros(d.getMonth() + 1, 2) + '-' +
+        leadingZeros(d.getDate(), 2) + ' ' +
+
+        leadingZeros(d.getHours(), 2) + ':' +
+        leadingZeros(d.getMinutes(), 2) + ':' +
+        leadingZeros(d.getSeconds(), 2);
+
+    return s;
+}
+
+function leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+
+    if (n.length < digits) {
+        for (i = 0; i < digits - n.length; i++)
+            zero += '0';
+    }
+    return zero + n;
+}
